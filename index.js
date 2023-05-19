47cfd43e695ed9db6cfbbbeafb18609172dd1b7b
Shalahuddin360 <shalahuddinislam792@gmail.com>
2023-05-19 18:40:47 +0600

    created initial setup with mongodb

--- index.js
+++ index.js
@@ -25,9 +25,15 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-     client.connect();
+    await client.connect(); //await
+   app.post("/postToy",(req,res)=>{
+    const body =req.body;
+    console.log(body)
+   })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
+
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
