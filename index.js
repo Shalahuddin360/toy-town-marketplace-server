eb11d8e340b478d72a42eae2cf65d70cbf7f7561
Shalahuddin360 <shalahuddinislam792@gmail.com>
2023-05-21 10:54:08 +0600

    some code optimized are added

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const express = require('express');
-const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config()
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
 const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware 
 app.use(cors());
-app.use(express.json());   
+app.use(express.json());
 
 
 
@@ -18,37 +19,75 @@ const client = new MongoClient(uri, {
     serverApi: {
         version: ServerApiVersion.v1,
         strict: true,
-        deprecationErrors: true,  
+        deprecationErrors: true,
     }
+    // useNewUrlParser:true,
+    // useUnifiedTopology:true,
+    // maxPoolSize:10
+
 });
 
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect(); //await
+        client.connect((err) => {
+            if (err) {
+                console.error(err);
+                return;
+            }
+        }); //await
         const toyCollection = client.db('toyDB').collection('toy');
 
-        app.post("/postToy",async (req,res) => {
-            const newToy = req.body;
-            // if(!newToy){
-            //     return res.status(404).send({message : 'newToy data not valid request'})
-            // }
-            console.log(newToy)
-            const result = await toyCollection.insertOne(newToy)
-            res.send(result)
-        })
-        app.get("/allToys/:text",async(req,res)=>{
+        app.get("/allToys/:text", async (req, res) => {
             console.log(req.params.text)
-            if(req.params.text=="scooter" ||req.params.text=="drone" ||req.params.text=="stroller" ){
-                const result = await toyCollection.find({status : req.params.text}).toArray();
+            if (req.params.text == "scooter" || req.params.text == "drone" || req.params.text == "stroller") {
+                const result = await toyCollection
+                    .find({ status: req.params.text })
+                    // .sort({price:"-1"})
+                    .toArray();
                 console.log(result)
                 return res.send(result);
             }
-            const result = await toyCollection.find({}).toArray();
+            const result = await toyCollection
+                .find({})
+                // .sort({price:"1"})
+                .toArray();
             res.send(result)
         })
 
+        //  app.get("/allToys/:id", async (req,res)=>{
+        //     const id =req.params.id;
+        //     const query ={_id : new ObjectId (id)}
+        //     const result = await toyCollection.findOne(query)
+        //     res.send(result)
+        //  })
 
+        app.post("/postToy", async (req, res) => {
+            const newToy = req.body;
+
+            if (!newToy) {
+                return res.status(404).send({ message: 'newToy data not valid request' })
+            }
+            // console.log(newToy)
+            const result = await toyCollection.insertOne(newToy)
+            res.send(result)
+        })
+
+        app.get("/singleToy/:id", async (req, res) => {
+            console.log(req.params.id);
+            const result = await toyCollection.findOne({
+              _id: new ObjectId(req.params.id),
+            });
+            res.send(result);
+          });
+
+        app.get("/myToys/:email", async (req, res) => {
+            console.log(req.params.email);
+            const result = await toyCollection.find({email: req.params.email}).toArray();
+            console.log(result)
+            res.send(result);
+         
+        });
 
 
 
